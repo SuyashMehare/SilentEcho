bc6462837d95dc0b39281725964f666269f9aab3
SuyashMehare <suyashmehare04@gmail.com>
2024-09-01 22:43:46 +0530

    Improved backend

--- src/lib/send-mail.util.ts
+++ src/lib/send-mail.util.ts
@@ -1,43 +1,36 @@
 import { EmailTemplate } from "@/components/email-template";
-import { ApiResponse } from "@/utility/ApiResponse";
+import { IApiResponse } from "@/types/ApiResponse";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 
-export default async function (username:string,to:string) {
+export default async function sendVerificationEmail(username:string,email:string) : Promise<IApiResponse>{
     
     const verifyCode : string = String(Math.floor(Math.random()*1000000))
 
     const { data , error } = await resend.emails.send({
         from: 'Acme <onboarding@resend.dev>',
-        to: to,
-        subject: 'Verfication code | SiltEcho',
+        to: email,
+        subject: 'SiltEcho | Verfication code',
         react: EmailTemplate({username,verifyCode})
     })
 
 
 
     if(error){
-        return Response.json(
-            ApiResponse({
-                success : false,
-                message : "Error while sending email",
-                status : 500
-            }),
-            {status:500}
-        )
+
+        console.error('Error sending verification email:', error);    
+        return {
+            success: false,
+            message: "Failed to send verification email",
+        }
     }
 
-    return Response.json(
-        ApiResponse({
-            success:true,
-            message: "Email send successfully",
-            status:200
-        }),
-        {
-            status:200
-        }
-    )
+
+    return {
+        success: true,
+        message: 'Verification email sent successfully'
+    }
 }
